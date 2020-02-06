0473cb55a2d46f30b82785e9ca475d16c8d741bb
Ubuntu <ubuntu@ip-172-31-47-221.ap-northeast-2.compute.internal>
2020-02-06 06:36:31 +0000

    Modify App

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ import { withAuthenticator } from 'aws-amplify-react';
 
 Amplify.configure(awsconfig);
 
-class App extends Component {}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
 
 const signUpConfig = {
   header: 'My Customized Sign Up',
